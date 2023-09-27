d9f3a48ad785eb811328721588ecb4291998322c
hyohyo814 <sojaisam@gmail.com>
2023-09-26 19:57:32 -0700

    SIGSEV somewher idk

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ type Album struct {
 }
 
 func main() {
+	// env
 	var (
 		User   = os.Getenv("DBUSER")
 		Passwd = os.Getenv("DBPASS")
@@ -27,7 +28,11 @@ func main() {
 
 	var err error
 	psqlInfo := fmt.Sprintf("user=%s password=%s dbname=%s", User, Passwd, DBName)
+
+	// Open db conn
 	db, err := sql.Open("postgres", psqlInfo)
+
+	// Err handling
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -38,4 +43,32 @@ func main() {
 	}
 
 	fmt.Println("Connected!")
+
+	albums, err := albumsByArtist("Jon")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
+}
+
+func albumsByArtist(name string) ([]Album, error) {
+	var albums []Album
+
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	return albums, nil
 }
